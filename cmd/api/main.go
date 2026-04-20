6d7904c9b7e016bd6a7f2b67ffb0b9c71e292cff
vinit-jpl <vinit.jpl@gmail.com>
2026-04-20 23:05:03 +0530

    add configuration loading functionality

---
+++ cmd/api/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"go-tweets/internal/config"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	r := gin.Default()
+
+	cfg, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	server := fmt.Sprintf("127.0.0.1:%s", cfg.Port)
+	r.Run(server)
+}
